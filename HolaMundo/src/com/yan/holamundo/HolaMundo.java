e48129a40d060fdf3326fb806aa853579c1668fb
YancarloOjeda <yanojedaps@gmail.com>
2020-06-24 09:56:51 -0500

    first commit

---
+++ HolaMundo/src/com/yan/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package com.yan.holamundo;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo .l.");	
+		short i = 0;
+		byte j =0;
+		int k =0;
+		long l = 0;
+		
+	}
+
+}
